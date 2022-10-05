31c84a08882de60f82eefc5a287c5a05e8d85801
Nathan Rooy <rooyna@mail.uc.edu>
2022-10-05 16:52:34 -0400

    minor updates

--- test/test_all.py
+++ test/test_all.py
@@ -1,25 +1,16 @@
 import unittest
-from random import choices, sample
 from turboshtein import levenshtein
-
-
-ascii = '''0123456789\
-abcdefghijklmnopqrstuvwxyz\
-ABCDEFGHIJKLMNOPQRSTUVWXYZ\
-!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
+from utils import create_strings
 
 
 class TestLevenshtein(unittest.TestCase):
     def test_ascii(self):
         for s_len in range(1, 64):
             for edit_dist in range(0, s_len + 1):
-                s1 = choices(ascii, k=s_len)
-                s2 = s1[:]
-                for idx in sample([i for i in range(len(s1))], edit_dist):
-                    s2[idx] = choices([c for c in ascii if c not in s1], k=1)[0]
+                s1, s2 = create_strings(s_len, edit_dist)
                 self.assertEqual(
                     edit_dist,
-                    levenshtein(''.join(s1), ''.join(s2))
+                    levenshtein(s1, s2)
                 )
 
 
